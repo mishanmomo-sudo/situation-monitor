c5ddcc7424539f9e0c932fff8bedbe7df5bcbcf3
mishanmomo-sudo <mishanmomo@gmail.com>
2026-01-29 11:34:39 +0800

    Update hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,26 +1,30 @@
 import { GATEWAY_TOKEN } from '$env/static/private';
 
-/** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-    const VALID_TOKEN = GATEWAY_TOKEN;
     const token = event.url.searchParams.get('token');
     const session = event.cookies.get('gateway_session');
 
-    if (token === VALID_TOKEN || session === 'authorized') {
+    console.log({
+        token,
+        session,
+        envToken: GATEWAY_TOKEN
+    });
+
+    if (token === GATEWAY_TOKEN || session === 'authorized') {
         const response = await resolve(event);
 
         response.headers.set(
-            'Content-Security-Policy',
-            "frame-ancestors 'self' https://mishan3.xyz https://www.mishan3.xyz;"
+          'Content-Security-Policy',
+          "frame-ancestors 'self' https://mishan3.xyz https://www.mishan3.xyz;"
         );
 
-        if (token === VALID_TOKEN && session !== 'authorized') {
+        if (token === GATEWAY_TOKEN && session !== 'authorized') {
             event.cookies.set('gateway_session', 'authorized', {
                 path: '/',
                 httpOnly: true,
                 sameSite: 'none',
                 secure: true,
-                maxAge: 60 * 60 * 24
+                maxAge: 86400
             });
         }
 
